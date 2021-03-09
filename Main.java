cab5bcf72b5539a2e3f73a3828dd5c124fc3e184
khekrncs <kishorenayark@gmail.com>
2021-03-09 12:06:27 +0530

    Solving advent of code in java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
-
-import hashtables.*;
+import array.*;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        var object = new FirstRepeatedCharacter();
-        System.out.println(object.firstRepeatedCharacter("A Green Apple"));
+        var object = new GameWithNOS();
+        var input = new int[]{5, 9, 7, 6};
+        System.out.println(Arrays.toString(object.game_with_number(input, input.length)));
     }
     
 }
