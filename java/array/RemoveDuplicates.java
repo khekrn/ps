bf851fb395ec2600e8ab5ed6dd6015a744527407
khekrn <kishorenayark@gmail.com>
2023-08-06 13:13:56 +0530

    Adding Static Array Problems

---
+++ java/array/RemoveDuplicates.java
@@ -0,0 +1,46 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove the 
+ * duplicates in-place such that each unique element appears only once. 
+ * The relative order of the elements should be kept the same. 
+ * Then return the number of unique elements in nums.
+ * 
+ * Consider the number of unique elements of nums to be k, to get accepted, 
+ * you need to do the following things:
+ *  - Change the array nums such that the first k elements of nums contain 
+ *    the unique elements in the order they were present in nums initially. 
+ *    The remaining elements of nums are not important as well as the 
+ *    size of nums.
+ *  - Return k.
+ * 
+ */
+
+public class RemoveDuplicates {
+
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+        if (nums.length == 1){
+            return 1;
+        }
+        var result = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[result] = nums[i]; 
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        var removeDuplicates = new RemoveDuplicates();
+        var arr = new int[]{1,1,2,2};
+        System.out.println("Result = "+removeDuplicates.removeDuplicates(arr));
+        System.out.println("Array = "+Arrays.toString(arr));
+    }
+}
---
+++ java/array/RemoveElement.java
@@ -0,0 +1,43 @@
+package array;
+
+/*
+ * Given an integer array nums and an integer val, remove all occurrences of 
+ * val in nums in-place. The order of the elements may be changed. Then 
+ * return the number of elements in nums which are not equal to val.
+ * Consider the number of elements in nums which are not equal to val be k, 
+ * to get accepted, you need to do the following things:
+ * - Change the array nums such that the first k elements of nums contain 
+ *   the elements which are not equal to val. The remaining elements of nums
+ *   are not important as well as the size of nums.
+ * - Return k.
+ */
+
+public class RemoveElement {
+
+    private void swap(int[] arr, int i, int j){
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public int removeElement(int[] nums, int val) {
+        var window = nums.length-1;
+        var index = 0;
+        while(index <= window){
+            if(nums[index] == val){
+                swap(nums, index, window);
+                window -=1;
+            }else{
+                index += 1;
+            }
+
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        var removeElement = new RemoveElement();
+        var nums = new int[]{3,2,2,3};
+        System.out.println(removeElement.removeElement(nums, 3));
+    }
+}
