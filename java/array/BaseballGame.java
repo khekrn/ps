16462b5c0070454be972441114fa014ca2578925
khekrn <kishorenayark@gmail.com>
2023-08-06 21:25:35 +0530

    Adding the baseball game problem in java

--- java/array/BaseballGame.java
+++ java/array/BaseballGame.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 /**
  *  Baseball Game
  *
+ * https://leetcode.com/problems/baseball-game/
  * You are keeping the scores for a baseball game with 
  * strange rules. At the beginning of the game, you start 
  * with an empty record.
